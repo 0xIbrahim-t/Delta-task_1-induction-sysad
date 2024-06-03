a60c5845dd21d77bec0b469c5a0174a7fd4930d7
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-04 00:35:36 +0530

    Update answerQuiz.sh

--- answerQuiz.sh
+++ answerQuiz.sh
@@ -2,7 +2,7 @@
 
 
 
-if ! [ -f ~/domain_pref.txt]; then
+if ! [ -f ~/domain_pref.txt ]; then
 	echo "Only a mentee can run this command to answer their questions by their mentor"
 	exit 1
 fi
@@ -10,9 +10,12 @@ fi
 answers=()
 question_number=1
 readarray -t questions_to_answer < ~/questions.txt
-for question_to_answer in ${question_to_answer[@]}; do
+
+for question_to_answer in "${questions_to_answer[@]}"; do
+	echo $question_to_answer
 	read -p "${question_to_answer}: " answer_to_the_question
 	answers+=("question-${question_number}: $answer_to_the_question")
+	question_number=$(($question_number + 1))
 done
 touch ~/quiz_answers
 chown :${USER}_group ~/quiz_answers
