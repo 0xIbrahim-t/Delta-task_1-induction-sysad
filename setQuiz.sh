21f4c7c853da65a9a028d99dabffddd4981b9d32
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-04 00:37:21 +0530

    Update setQuiz.sh

--- setQuiz.sh
+++ setQuiz.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-if [ -d ~/submittedTasks]; then
+if [ -d ~/submittedTasks ]; then
 	PS3="Choose an option below: "
 	question_number=1
-	option_1='do you set a question to your mentee/mentees?'
+	option_1='do you want to set a question to your mentee/mentees?'
 	questions=()
-	select option in option_1 'exit' 'exit without setting the quiz'; do
+	select option in "$option_1" 'done' 'exit without setting the quiz'; do
 		case $option in
 			$option_1)
-				read -p "Enter the question number $question_number: " question
+				read -p "Enter the question $question_number: " question
 				questions+=$question
 				;;
 				
-			'exit')
-				for allocated_mentee_info in ${allocated_mentees[@]}; do
-					if [$allocated_mentee_info == "rollnumber name domain"]; then
-						continue
-					fi
-					read -a allocated_mentee_info_array <<< "$allocated_mentee_info"
-					allocated_mentees+=("${allocated_mentee_info_array[1]}")
-				done
+			'done')
+				readarray -t allocated_mentees < <(awk '{print $2}' allocatedMentees.txt)
 				for allocated_mentee in ${allocated_mentees[@]}; do
-					if [ -f $(eval echo ~$allocated_mentee)/questions.txt]; then
+					echo $allocated_mentee
+					if [ -f $(eval echo ~$allocated_mentee)/questions.txt ]; then
 						rm $(eval echo ~$allocated_mentee)/questions.txt
 					fi
 					touch $(eval echo ~$allocated_mentee)/questions.txt
 					chown :${sllocated_mentee}_group $(eval echo ~$allocated_mentee)/questions.txt
-					chmod -R 770 $(eval echo ~$allocated_mentee)/questions.txt
+					chmod -R 774 $(eval echo ~$allocated_mentee)/questions.txt
 					for qn in ${questions[@]}; do
 						echo "${qn}" >> $(eval echo ~$allocated_mentee)/questions.txt
 					done
@@ -35,6 +30,7 @@ if [ -d ~/submittedTasks]; then
 					echo "/script/delete_noti.sh" >> $(eval echo ~$allocated_mentee)/.bashrc
 					echo "The quiz has been successfully set for this $allocated_mentee and the notification has been sent."
 				done
+				question_number=$(($question_number + 1))
 				break
 				;;
 
@@ -42,6 +38,7 @@ if [ -d ~/submittedTasks]; then
 				exit 0
 				break
 				;;
+		esac
 	done
 else
 	echo "Only a mentor can run this alias to set questions for their mentees."
