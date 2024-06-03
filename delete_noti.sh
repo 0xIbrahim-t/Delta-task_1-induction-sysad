880bba30a8bc46b3ecc6b831778d3adeacaa883c
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-04 00:44:54 +0530

    Update delete_noti.sh

--- delete_noti.sh
+++ delete_noti.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-sed -i '/\/script\/notification.sh/d' ~/.bashrc
-sed -i '/\/script\/delete_noti.sh/d' ~/.bashrc
+sed -i '/\/scripts\/notification.sh/d' ~/.bashrc
+sed -i '/\/scripts\/delete_noti.sh/d' ~/.bashrc
