2d336f8cb3286e82dbf10518e2ed16e4a373e390
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-04 00:02:05 +0530

    Update deRegister.sh

--- deRegister.sh
+++ deRegister.sh
@@ -2,19 +2,20 @@
 
 
 
-if [ -f ~/domain_pref.txt]; then
+if [ -f ~/domain_pref.txt ]; then
 	PS3="Select the domain you want to deregister from your induction process: "
 	readarray -t domains_registered < ~/domain_pref.txt
 	select domain_to_deregister in ${domains_registered[@]}; do
-		rm -R ~/domain_to_deregister
+		rm -R ~/$domain_to_deregister
 		rm ~/domain_pref.txt
 		touch ~/domain_pref.txt
 		for domain_registered in ${domains_registered[@]}; do
-			if ["$domain_registered"=="$domain_to_deregister"]; then
+			if [ "$domain_registered" == "$domain_to_deregister" ]; then
 				continue
 			fi
-			echo "domain_registered" >> ~/domain_pref.txt
+			echo "$domain_registered" >> ~/domain_pref.txt
 		done
+		break
 	done
 else
 	echo "You should be an mentee to run this command to deregister from this induction process."
