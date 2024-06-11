91cd9c2bccbcd8ad95a32ff1cddecc1e80e77f09
0xIbrahim T <85542378+0xIbrahim-t@users.noreply.github.com>
2024-06-12 00:35:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ mkdir /scripts
 script_files=(check_deregistered.sh cronjob.sh deRegister.sh delete_noti.sh displayStatus.sh domainPref.sh mentorAllocation.sh notification.sh setQuiz.sh submitTask.sh userGen.sh answerQuiz.sh)
 for script_file in ${script_files[@]}; do
 	mv $script_file /scripts
-	chmod -R 711 /scripts/$sccript_file
+	chmod -R 711 /scripts/$script_file
 done
 
 alias_script_files=(deRegister.sh displayStatus.sh domainPref.sh mentorAllocation.sh setQuiz.sh submitTask.sh userGen.sh answerQuiz.sh)
